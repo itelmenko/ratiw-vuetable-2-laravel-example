b8dc9f677848b7b5580e14f7ea3014550a9236d1
Igor Telmenko <izumeroot@gmail.com>
2017-05-10 09:08:26 +0300

    Css cleaning

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,7 +24,7 @@
         </script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div>
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
